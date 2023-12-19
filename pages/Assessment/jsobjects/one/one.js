0750d4da6ae61976dac6014cf7c5451e572ff8d9
sumit.singhal <sumit.singhal@salesforce.com>
2023-12-19 07:57:57 +0000

    Change in get method

--- pages/Assessment/jsobjects/one/one.js
+++ pages/Assessment/jsobjects/one/one.js
@@ -17,4 +17,33 @@ export default {
 			return this.QuestionTextjson;
 		}
 	},
+	fetchElementDetails () {
+		get_DFTOS.run();
+		for (let i = 0; i < get_DFTOS.data.elements.length; i++) {
+			if(get_DFTOS.data.elements[i].type === "Step"){
+				var stepElement = get_DFTOS.data.elements[i].name;
+				this.QuestionTextjson[stepElement] = {};
+				for (let j = 0; j < get_DFTOS.data.elements[i].elements.length; j++) {		
+					var currentElement = get_DFTOS.data.elements[i].elements[j];
+					var Questiontext  = currentElement.customTypeDetails.discoveryFramework.questionText.toString();
+					var QuestionDev  = currentElement.customTypeDetails.discoveryFramework.questionDeveloperName.toString();
+					if(currentElement.data.elements.length === 0)
+					{
+						this.QuestionTextjson[stepElement][Questiontext] = "";
+					}
+					else{
+						this.QuestionTextjson[stepElement][Questiontext] = {};
+						for (let k = 0; k < currentElement.data.elements[i].elements.length; k++) {		
+							var childElement = currentElement.data.elements[i].elements[j];
+							var childQuestiontext  = childElement.customTypeDetails.discoveryFramework.questionText.toString();
+							var childQuestionDev  = childElement.customTypeDetails.discoveryFramework.questionDeveloperName.toString();
+							this.QuestionTextjson[stepElement][Questiontext][childQuestiontext] = "";
+					  }
+				}
+				console.log(this.QuestionDeveloperName);
+			}
+			
+			return this.QuestionTextjson;
+		}
+	}
 }
